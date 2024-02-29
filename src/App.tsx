7a3ec29f50382cc4e5aedef6fd8469d4198156dd
Carlos Molina A <hi@cmolina.dev>
2024-02-29 14:07:18 -0300

    Upgrade to nodejs20

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import { FormControl, FormLabel, RadioGroup, FormControlLabel, Radio, Typography, Button, FormHelperText } from '@mui/material'
 import Grid from '@mui/material/Unstable_Grid2'
